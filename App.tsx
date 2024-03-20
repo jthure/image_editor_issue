69bf54b3163afc70c89171bc4cc560912ff45718
Jonas Thuresson <jonas.thuresson@klarna.com>
2024-03-20 08:32:33 +0100

    Initial commit
    Generated by react-native@0.73.6

---
+++ App.tsx
@@ -0,0 +1,88 @@
+import {StyleSheet, View, Image, useWindowDimensions} from 'react-native';
+import ImageEditor from '@react-native-community/image-editor';
+import React, {useCallback, useEffect, useState} from 'react';
+import {copyFile, PicturesDirectoryPath} from 'react-native-fs';
+
+export default function App() {
+  const dimensions = useWindowDimensions();
+  const [scaledImage1, scaledImage2] = useScaledImage() || [];
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('./image.jpeg')}
+        style={{width: dimensions.width, flex: 1}}
+        resizeMode="contain"
+      />
+      {scaledImage1 && (
+        <Image
+          source={{uri: scaledImage1}}
+          style={{width: dimensions.width, flex: 1}}
+          resizeMode="contain"
+        />
+      )}
+      {scaledImage2 && (
+        <Image
+          source={{uri: scaledImage2}}
+          style={{width: dimensions.width, flex: 1}}
+          resizeMode="contain"
+        />
+      )}
+    </View>
+  );
+}
+
+const useScaledImage = () => {
+  const [scaledImage, setScaledImage] = useState<
+    readonly [string, string] | null
+  >(null);
+
+  const scaleImage = useCallback(async () => {
+    const asset = Image.resolveAssetSource(require('./image.jpeg'));
+    const {width, height} = await new Promise<{
+      width: number;
+      height: number;
+    }>((resolve, reject) =>
+      Image.getSize(
+        asset.uri,
+        (_width, _height) => resolve({width: _width, height: _height}),
+        reject,
+      ),
+    );
+    const cropResult1 = await ImageEditor.cropImage(asset.uri, {
+      offset: {x: 0, y: 0},
+      size: {width, height},
+    });
+
+    const scaleFactor = 200 / width;
+    const displaySize = {
+      width: width * scaleFactor,
+      height: height * scaleFactor,
+    };
+
+    const cropResult2 = await ImageEditor.cropImage(asset.uri, {
+      offset: {x: 0, y: 0},
+      size: {width, height},
+      displaySize,
+    });
+
+    await Promise.all([
+      copyFile(cropResult1.uri, `${PicturesDirectoryPath}/crop1.jpg`),
+      copyFile(cropResult2.uri, `${PicturesDirectoryPath}/crop2.jpg`),
+    ]);
+
+    setScaledImage([cropResult1.uri, cropResult2.uri]);
+  }, []);
+  useEffect(() => {
+    scaleImage();
+  }, [scaleImage]);
+
+  return scaledImage;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+});
